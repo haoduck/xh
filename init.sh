15295c027ba457bffb0dd14d6e30fb88ea5fd2e3
haoduck <56804964+haoduck@users.noreply.github.com>
2022-03-24 04:28:40 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -17,6 +17,40 @@ cat << EOF > /vv
                 "path": "${WSPATH}"
             }
         }
+    },
+    {
+        "port": 80,
+        "protocol": "vless",
+        "settings": {
+            "clients": [{
+                "id": "${ID}",
+                "alterId": ${AID}
+            }],
+            "decryption": "none"
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "${WSPATH}"
+            }
+        }
+    },
+    {
+        "port": 8080,
+        "protocol": "vless",
+        "settings": {
+            "clients": [{
+                "id": "${ID}",
+                "alterId": ${AID}
+            }],
+            "decryption": "none"
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "${WSPATH}"
+            }
+        }
     }],
     "outbounds": [{
         "protocol": "freedom"
