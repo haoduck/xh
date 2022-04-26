42cffe6a41bc6a885d592cf186e517516e5bc7e8
haoduck <56804964+haoduck@users.noreply.github.com>
2022-04-26 14:44:47 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,29 @@
+cat << EOF > /x.json
+{
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "b831381d-6324-4d53-ad4f-8cda48b30811"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+
+xray -c /x.json
